93820ec3869b242adc68e4865f2dce6161f672c6
CodingManoj <CodingManoj@gmail.com>
2024-11-10 09:41:32 -0500

    Building EKS

--- installHelmCharts.tf
+++ installHelmCharts.tf
@@ -1,7 +1,9 @@
 resource "null_resource" "helm_install" {
 
   depends_on = [aws_eks_cluster.main, aws_eks_node_group.node]
-
+  triggers = {
+    always_run = timestamp() # This ensure that this provisioner would be triggering all the time
+  }
   provisioner "local-exec" {
     command = <<EOF
 aws eks update-kubeconfig --name "${var.env}-eks"
