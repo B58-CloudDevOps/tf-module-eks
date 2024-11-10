78acf72390495559cbc2151f6539cae37b484990
CodingManoj <CodingManoj@gmail.com>
2024-11-10 09:44:23 -0500

    Building EKS

--- installHelmCharts.tf
+++ installHelmCharts.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "helm_install" {
 
-  depends_on = [aws_eks_cluster.main, aws_eks_node_group.node]
+  #   depends_on = [aws_eks_cluster.main, aws_eks_node_group.node]
   triggers = {
     always_run = timestamp() # This ensure that this provisioner would be triggering all the time
   }
