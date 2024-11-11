0f11b1c7934e405e2563b9662679cab4e9f3df38
CodingManoj <CodingManoj@gmail.com>
2024-11-10 19:42:42 -0500

    Kubernetes AutoPush

--- addOn.tf
+++ addOn.tf
@@ -1,6 +1,6 @@
 # This enables the VPC-CNI 
 resource "aws_eks_addon" "vpc_cni" {
-  depends_on   = [aws_eks_cluster.main]
+  # depends_on   = [aws_eks_cluster.main]
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "vpc-cni"
   configuration_values = jsonencode({
