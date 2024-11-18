a62a7785600746006878690477a6c168b49bc8ae
CodingManoj <CodingManoj@gmail.com>
2024-11-17 19:46:12 -0500

    Kubernetes AutoPush

--- installHelmCharts.tf
+++ installHelmCharts.tf
@@ -7,13 +7,14 @@ resource "null_resource" "helm_install_boot" {
   provisioner "local-exec" {
     command = <<EOF
 rm -rf .kube/config
+sleep 240
 aws eks update-kubeconfig --name "${var.env}-eks"
 kubectl get nodes
 echo "Installing Metrics Server"
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 echo "Installing ArgoCD"
 kubectl create ns argocd && true
-sleep 15
+sleep 30
 kubectl apply -f https://raw.githubusercontent.com/B58-CloudDevOps/learn-kubernetes/refs/heads/main/arogCD/argo.yaml -n argocd 
 EOF
   }
