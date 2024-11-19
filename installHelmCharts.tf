ddebc5273329374035e773ab1035c3d7be2e7e26
CodingManoj <CodingManoj@gmail.com>
2024-11-18 22:19:52 -0500

    CI Job Name

--- installHelmCharts.tf
+++ installHelmCharts.tf
@@ -26,3 +26,24 @@ helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ${path.module}/ingress
 EOF
   }
 }
+
+
+
+# Destroy time provisioner to delete the lb
+resource "null_resource" "helm_uninstall" {
+
+  #   depends_on = [aws_eks_cluster.main, aws_eks_node_group.node]
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOF
+rm -rf .kube/config
+aws eks update-kubeconfig --name "${var.env}-eks"
+echo "UnInstalling Nginx Ingress Controller"
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+ls -ltr
+echo "${path.module}"
+helm uninstall ngx-ingress
+helm list
+EOF
+  }
+}
